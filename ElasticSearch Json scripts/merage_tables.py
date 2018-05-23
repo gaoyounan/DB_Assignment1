62681412fef0893fa58b982286be57644c73f909
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-23 15:48:48 -0300

    Change the script to marge the original CSV files

--- ElasticSearch Json scripts/merage_tables.py
+++ ElasticSearch Json scripts/merage_tables.py
@@ -1,7 +1,14 @@
 import pandas as pd
-df_trip = pd.read_csv(".\\CSV_files\\busDB_trips.csv")
-df_stime = pd.read_csv(".\\CSV_files\\busDB_stoptimes.csv")
 
-df_merg = pd.merge(df_trip, df_stime, on=["trip_id_", "trip_id"])
+df_stops = pd.read_csv("./CSV_files/stops.csv")
+df_stime = pd.read_csv("./CSV_files/stoptimes.csv")
+df_trips = pd.read_csv("./CSV_files/trips.csv")
 
+df_stops_stime=pd.merge(df_stops,df_stime,on=["stop_id"])
+
+df_stops_stime=df_stops_stime.drop(['stop_id','lat','log'],axis=1)
+
+df_merg = pd.merge(df_trips, df_stops_stime, on=["trip_id"])
+df_merg =df_merg.drop(['block_id'],axis=1)
+df_merg['id']=df_merg.index+1
 df_merg.to_csv("mrget.csv", index=False)
